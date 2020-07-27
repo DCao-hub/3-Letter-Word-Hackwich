7bb7b7ef1b1eb7b1a5f3cbbfee7ee5252a80ce2d
diana <dianacao777@gmail.com>
2020-07-27 12:50:02 -0400

    Initial Commit

---
+++ 3 Letter Word Hackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3 Letter Word Hackwich
+//
+//  Created by Diana Cao on 7/27/20.
+//  Copyright © 2020 Diana Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
